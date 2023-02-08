04663c6d6cdbc4c8f532360ecd771cad2aba612a
EvanilsonP <pevanilson99@gmail.com>
2023-02-08 11:25:23 -0300

    Operators PT.2

--- 2-operators.js
+++ 2-operators.js
@@ -1,10 +1,49 @@
-// Operators
-let f = 3 % 2 + 4 / 4
-console.log(f); // 2
+// // OPERATORS
+// let f = 3 % 2 + 4 / 4
+// console.log(f); // 2
 
-// Assignment operators
-let number = 8;
-number += 2;
-number %= 2; 
+// // ASSIGNMENT OPERATORS
+// let number = 8;
+// number += 2;
+// number %= 2; 
 
-console.log(number);
+// console.log(number);
+
+// // RELATIONAL OPERATORS
+// let a = 5 > 2 // true
+// let b = 3 < 2 // false
+// let c = 5 >= 2 // true
+// let d = 10 == 10 // true
+// let e = 5 != 2 // true
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// LOGICAL OPERATORS
+// let time = 'Night';
+
+// let sunday = true;
+// let footbal = true;
+// let today = 'monday';
+
+// if(time != 'Night') { // not equal to
+//     console.log("It's a pretty sunny day.");
+// };
+
+// if(sunday && footbal != today) { // and
+//     console.log('Sorry, no footbal today.');
+// }
+
+// let n1 = 2;
+// let n2 = 2;
+
+// let sum = n1 >= n2 || 2 + 2; // || or
+// console.log(sum);
+
+// TERNARY OPERATOR
+let average = 10;
+average >= 7.0 ? 'Passed' : 'Failed';
+// Passed
