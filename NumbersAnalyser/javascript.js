ecb14e06a68ad881de9cf195dbc45f0056db5bc8
EvanilsonP <pevanilson99@gmail.com>
2023-02-11 21:00:18 -0300

    Finishing up

--- NumbersAnalyser/javascript.js
+++ NumbersAnalyser/javascript.js
@@ -22,8 +22,46 @@ function inLista(n, i) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        alert('kpw')
+        valores.push(Number(num.value));
+        let item = document.createElement('option');
+        item.text = `Valor ${num.value} adicionado.`;
+        lista.appendChild(item);
+        result.innerHTML = '';
     } else {
         alert('Valor inválido ou já encontrado na lista.');
     }
+    num.value = '';
+    num.focus();
 };
+
+function Finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar.');
+    } else {
+        let tot = valores.length;
+        let menor = valores[0];
+        let maior = valores[0];
+        let soma = 0;
+        let media = 0;
+
+        for(let pos in valores) {
+            soma += valores[pos]
+            if(valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+
+        media = soma / tot
+        result.innerHTML = '';
+        result.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+        result.innerHTML += `<p>O maior número cadastrado foi ${maior}.</p>`;
+        result.innerHTML += `<p>O menor número cadastrado foi ${menor}.</p>`;
+        result.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`;
+        result.innerHTML += `<p>A média dos valores é ${media}`;
+
+
+    }
+}
